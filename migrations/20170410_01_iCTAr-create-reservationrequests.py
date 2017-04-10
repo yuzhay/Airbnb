794c8e476cfe48d155b9bbec8ddf4012206a623a
Yuriy Zhayvoronok <yura@zhayvoronok.com>
2017-04-10 23:59:42 +0300

    Draft

---
+++ migrations/20170410_01_iCTAr-create-reservationrequests.py
@@ -0,0 +1,39 @@
+"""
+Create ReservationRequests
+"""
+
+from yoyo import step
+
+__depends__ = {'20170409_01_nINMY-create-threads'}
+
+steps = [
+    step("""
+        CREATE TABLE reservation_requests (
+            id                          integer NOT NULL PRIMARY KEY,
+            thread_id                   integer NOT NULL,
+            inquiry_checkin_date        date NOT NULL,
+            inquiry_checkout_date       date NOT NULL,
+            inquiry_number_of_guests    integer NOT NULL,
+            inquiry_price_native        real NOT NULL,
+            listing_id                  integer NOT NULL,
+            price                       real NOT NULL,
+            guest_id                     integer NOT NULL,
+            currency                    varchar(16) NOT NULL,
+            instant_bookable            boolean NOT NULL,
+            is_superhost                boolean NOT NULL,
+            identity_verified           boolean NOT NULL,
+            guest_created_at            date NOT NULL,
+            status                      varchar(16) NOT NULL,
+
+            updated_at  timestamp without time zone default (now() at time zone 'utc'),
+            created_at  timestamp without time zone default (now() at time zone 'utc'),
+
+            CONSTRAINT fk_reservation_requests_thread_id FOREIGN KEY (thread_id)
+            REFERENCES threads (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+
+            CONSTRAINT fk_reservation_requests_listing_id FOREIGN KEY (listing_id)
+            REFERENCES listings (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+    )""",
+    "DROP TABLE reservation_requests"
+    )
+]
---
+++ models/reservation_request.py
@@ -0,0 +1,41 @@
+class ReservationRequest:
+
+    @staticmethod
+    def create(
+        db,
+        id,
+        thread_id,
+        inquiry_checkin_date,
+        inquiry_checkout_date,
+        inquiry_number_of_guests,
+        inquiry_price_native,
+        listing_id,
+        price,
+        guest_id,
+        currency,
+        instant_bookable,
+        is_superhost,
+        identity_verified,
+        guest_created_at,
+        status):
+
+        request = "INSERT INTO reservation_requests "
+                    + "(id, thread_id, inquiry_checkin_date, inquiry_checkout_date,"
+                    + "inquiry_number_of_guests, inquiry_price_native, listing_id,"
+                    + "price, guest_id, currency, instant_bookable, is_superhost,"
+                    + "dentity_verified, guest_created_at, status) "
+                    + "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+        return db.execute(
+            request,
+            (
+                id, thread_id, inquiry_checkin_date, inquiry_checkout_date,
+                inquiry_number_of_guests, inquiry_price_native, listing_id,
+                price, guest_id, currency, instant_bookable, is_superhost,
+                dentity_verified, guest_created_at, status
+            )
+        )
+
+    @staticmethod
+    def exists(db, id):
+        result = db.execute("SELECT id FROM reservation_requests WHERE id = %s", [id])
+        return result.rowcount > 0
